f9f5c695fd40d087de57e42c9e085db34062ed6d
a71143055 <a71143055@gmail.com>
2025-03-26 11:46:15 +0900

    FirstStringboot

--- src/main/java/kr/ac/kopo/jeong/firststringboot/controller/Example02Controller.java
+++ src/main/java/kr/ac/kopo/jeong/firststringboot/controller/Example02Controller.java
@@ -2,6 +2,7 @@ package kr.ac.kopo.jeong.firststringboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class Example02Controller {
     @RequestMapping(value = "Exam02", method = RequestMethod.GET)
     public String requestMethod() {
         return "@RestController 연습중입니다.";
+    }
+
+    @RequestMapping(value = "Exam03", method = RequestMethod.GET)
+    public @ResponseBody String getMessage() {
+        return "@RestController getMessage 연습";
     }
 }
